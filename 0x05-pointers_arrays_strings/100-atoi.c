0bd8ef9345c9b5550f8defa82988237821294ff6
SimonN97 <snwnjuguna97@gmail.com>
2022-07-11 11:30:12 -0700

    My Commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,44 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - generates random passwords for 101-crackme
- * Return: zero
+ * _atoi - converts a string to an integer.
+ * @s: pointer to string.
+ * Return: integer gotten.
  */
-int main(void)
+int _atoi(char *s)
 {
-	int sum;
-	char c;
+	int index, ind2;
+	unsigned int res;
+	int sign = 1;
+	char now;
 
-	srand(time(NULL));
-	while (sum <= 2645)
+	index = 0;
+	res = 0;
+
+	while (*(s + index) != '\0')
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		now = *(s + index);
+		if (now == '-')
+		{
+			sign *= -1;
+		}
+		if (now >= '0' && now <= '9')
+		{
+			ind2 = index;
+			
+			while (*(s + ind2) > 47 && *(s + ind2) < 58)
+			{
+				res = (res * 10) + *(s + ind2) - '0';
+				ind2++;
+			}
+			break;
+		}
+		index++;
 	}
-	putchar(2772 - sum);
-	return (0);
+	if (sign < 0)
+		res *= sign;
+	return (res);
 }
+
+
